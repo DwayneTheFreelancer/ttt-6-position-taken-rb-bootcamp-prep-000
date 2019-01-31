a98d35e90b748934f2fdfb8db20b5d8daaef5a00
Dwayne Munro <dwaynemunro1845@gmail.com>
2019-01-31 18:46:16 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 # code your #position_taken? method here!
 
-def position_taken(board, idx) 
-  if(board[idx] == " ")
+def position_taken(board, index) 
+  if(board[index] == " ")
     return false 
   end
 end
